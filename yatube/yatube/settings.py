9da2353615a0445ab056d9e19d5264e12a84e89c
Николай Боровков <nikolai.borovkov@gmail.com>
2022-05-30 23:15:54 +0300

    CSRF_FAILURE_VIEW

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -129,12 +129,11 @@ CACHE_TIME = 20
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:home'
 # LOGOUT_REDIRECT_URL = 'posts:home'
-
-
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
 #  подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 # указываем директорию, в которую будут складываться файлы писем
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
